797b3b058f68821c9f356bea536f651c35df7fd2
AlanCompton <alan.compton@codeimmersives.com>
2022-10-12 14:39:20 -0500

    main.js assignment complete

--- main.js
+++ main.js
@@ -1,8 +1,56 @@
 // Your code here.
 
+// getFirstName
+function getFirstName(person){
+  return person.firstName;
+}
 
+// getLastName
+function getLastName(person){
+  return person.lastName;
+}
 
+//getFullName
+function getFullName(person){
+  return person.firstName+ " " + person.lastName;
+}
 
+// setFirstName
+function setFirstName(person,newFirstName){
+  person.firstName = newFirstName;
+}
+
+// setAge
+function setAge(person,newAge){
+  person.age = newAge;
+}
+
+// giveBirthday
+function giveBirthday(person){
+  if(person.age === undefined){
+    person.age = 1;
+  }else{
+    person.age += 1;
+  }
+}
+
+// marry
+function marry(person1,person2){
+  person1.married = true;
+  person2.married = true;
+
+  person1.spouseName = person2.firstName + " " + person2.lastName;
+  person2.spouseName = person1.firstName + " " + person1.lastName;
+}
+
+// divorce
+function divorce(person1,person2){
+  person1.married = false;
+  person2.married = false;
+
+  delete person1.spouseName;
+  delete person2.spouseName;
+}
 
 // Our code here. Don't touch!
 if (typeof getFirstName === 'undefined') {
